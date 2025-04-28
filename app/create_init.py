7aed2367a66703d4f4f6de10c313ad9adff4d0f9
mestevez22 <margiselle.estevez@gmail.com>
2025-04-28 15:04:26 -0400

    fixed clean function to properly change any ft measurements to yrds

--- app/create_init.py
+++ app/create_init.py
@@ -25,23 +25,26 @@ def clean(file):
     clean = df.loc[:, (df!=0).all(axis=0)].copy() #drop zero cols
     clean['Date'] = dt 
     clean['DistanceToPin_Yrds'] = clean['DistanceToPin'].apply(lambda x: round(float(x.split(" ")[0])/3, 2) if 'ft' in str(x.split(" ")[1]) else x) #change to yrds
-    clean['DistanceToPin_Yrds'] = clean['DistanceToPin'].apply(lambda x: str(x.split(" ")[1]) .replace("ft", "yds")) #change to yrds
+    clean['DistanceToPin_Yrds'] = clean['DistanceToPin'].apply(lambda x: f"{x.split(" "[0])} yds" if "ft" in x else x)
     clean = clean.drop('DistanceToPin', axis = 1)
     return clean
 
+
+
 ############## DO NOT RERUN - CREATION OF INITIAL MASTER DATA AND LOG FILE ###############
-#set paths 
+##set paths 
 # base_path = Path(__file__).resolve().parent #path of script
 # csv_path = base_path.parent #path of csv files
 # out_path = base_path / "master.csv" #path for output 
 
-#clean and merge csv files
+# #clean and merge csv files
 # try:
 #     files = [f for f in os.listdir(csv_path) if f.endswith(".csv")]
 #     all_dfs =[]
 #     for i, f in enumerate(files):
-#         #df = pd.read_csv(f)
-#         df = clean(f)
+#         file_path = csv_path / f
+#         #print(file_path)
+#         df = clean(file_path)
 #         if i != 0:
 #             df = df.iloc[1:, :] #drop header row 
 #         all_dfs.append(df)
