7fa742c73da5f342e8f66d19e3384f52e4af3b20
mestevez22 <margiselle.estevez@gmail.com>
2025-04-29 15:26:24 -0400

    add main page to streamlit dash

---
+++ app/app.py
@@ -0,0 +1,24 @@
+# Load packages
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import altair as alt
+import plotly.express as px
+from pathlib import Path
+from style import custom_sidebar_css
+
+
+#page setup
+st.set_page_config(
+    page_title="SimShot Analytics",
+    page_icon="⛳",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+custom_sidebar_css() #apply custom css for sidebar
+
+st.write("# Welcome to SimShot Analytics 👋")
+with st.sidebar:
+    st.success("Choose a view above")
