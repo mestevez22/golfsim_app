8a8fd0dbc6fb73546b11b5d5d590a0c61af0d79b
mestevez22 <margiselle.estevez@gmail.com>
2025-05-01 20:05:53 -0400

    add smash factor tab and analysis

--- app/pages/Sessions.py
+++ app/pages/Sessions.py
@@ -18,11 +18,12 @@ class SessionsPage:
     def __init__(self):
         self.data = Preprocessor().get_data()
         self.tab_labels = ['Performance', 'Angle of Attack', 'Smash Factor', 'Spin Analysis'] #tab labels
-        self.club_filter_opts = ['All'] + sorted(self.data['Club'].unique()) #club type filter options 
+        self.club_filter_opts = ['All'] + list(self.data['Club'].unique()) #club type filter options 
         self.start_date = self.data['Date'].min()
         self.end_date = self.data['Date'].max() + pd.Timedelta(days=1) - pd.Timedelta(seconds=1)
         self.not_metrics = ['Date', 'Unnamed: 0', 'Club', 'Decent']
         self.metric_opts = [i for i in set(self.data.columns) if i not in self.not_metrics]
+        self.spin_metrics = ['BackSpin', 'SideSpin', 'rawSpinAxis']
         self.filtered_data = None
     
     #filter data based on inputs
@@ -30,7 +31,9 @@ class SessionsPage:
         filtered = self.data[self.data['Date'].between(pd.to_datetime(date_range[0]), pd.to_datetime(date_range[1]))]
 
         if isinstance(selected_club, list):
-            if 'All' not in selected_club:
+            if 'All' in selected_club and len(selected_club) == 1:
+                pass  
+            else:
                 filtered = filtered[filtered['Club'].isin(selected_club)]
         else:
             if selected_club != 'All':
@@ -38,16 +41,21 @@ class SessionsPage:
 
         return filtered
 
+
     
     #define multiselection filter for plotly charts 
     def club_multiselect(self, key: str):
-        club_multiselect = st.multiselect(
-                    "Filter clubs", 
-                    self.club_filter_opts, 
-                    default=self.club_filter_opts,
-                    key=key
-                )
-        return club_multiselect
+        clubs = st.multiselect(
+            "Filter clubs", 
+            self.club_filter_opts, 
+            default=self.club_filter_opts,
+            key=key
+        )
+        if 'All' in clubs:
+            return list(self.data['Club'].unique())
+        return clubs
+
+
 
     #render tabs 
     def render(self):
@@ -144,8 +152,10 @@ class SessionsPage:
 
     ############## RENDER TAB 2 - ANGLE OF ATTACK ANALYSIS ############## 
     def render_AoA_tab(self):
-        st.markdown("## Angle of Attack vs. Club Type")
+        st.markdown("Angle of Attack vs. Club Type")
         cols = st.columns((4, 4), gap='medium')
+        all_club_chart = self.filter_data(self.date_range, 'All')  
+        aoa_range = [all_club_chart['AoA'].min() - 1, all_club_chart['AoA'].max() + 1]
     
 
         #-----chart 1: AoA vs Peak & Carry-------#
@@ -154,9 +164,7 @@ class SessionsPage:
             selected_clubs = self.club_multiselect("AoA_chart_club_filter")
 
             #filter based on club selection
-            filtered_chart = self.filter_data(self.date_range, selected_clubs)
-            aoa_range = [filtered_chart['AoA'].min() - 1, filtered_chart['AoA'].max() + 1]
-
+            filtered_chart = self.filter_data(self.date_range, selected_clubs)   #chart 1 only
 
             #create secondary y-axis
             fig = make_subplots(specs=[[{"secondary_y": True}]])
@@ -183,11 +191,10 @@ class SessionsPage:
             fig.update_yaxes(title_text="<b>Carry Distance</b>", secondary_y=False)
             fig.update_yaxes(title_text="<b>Peak Height</b> ", secondary_y=True)
             st.plotly_chart(fig, use_container_width=True)
-    
-            #-----chart 2: AoA vs Club -------#
-            #group by club, get mean AoA and count
+
+        #-----chart 2: AoA vs Club -------#
             grouped = (
-                filtered_chart
+                all_club_chart
                 .groupby('Club', observed=True)
                 .agg(AoA=('AoA', 'mean'), Count=('AoA', 'count'))
                 .reset_index()
@@ -208,27 +215,77 @@ class SessionsPage:
             fig2.update_yaxes(range= aoa_range)
             st.plotly_chart(fig2, use_container_width=True)
             
-            #-----chart 3: AoA vs Club -------#
-            with cols[1]:
-                st.markdown("Angle of Attacks vs. Carry Distance")
-                
-                # Create bins 
-                bin_edges = [0, 50, 75, 100, 125, 150, 175, 200,225]
-                bin_labels = ['0–50', '50–75', '75–100', '100-125', '125-150', '150–175', '175–200', '200-225']
-                filtered_chart = filtered_chart.copy()
-                filtered_chart['CarryBin'] = pd.cut(filtered_chart['Carry'], bins=bin_edges, labels=bin_labels, right=False)
-                binned = filtered_chart.groupby('CarryBin', observed=True).agg({'AoA': 'mean'}).reset_index()
+        #-----chart 3: AoA vs Club -------#
+        with cols[1]:
+            st.markdown("Angle of Attacks vs. Carry Distance")
+            
+            #create bins 
+            bin_edges = [0, 50, 75, 100, 125, 150, 175, 200,225]
+            bin_labels = ['0–50', '50–75', '75–100', '100-125', '125-150', '150–175', '175–200', '200-225']
+            all_club_chart = all_club_chart.copy()
+            all_club_chart['CarryBin'] = pd.cut(all_club_chart['Carry'], bins=bin_edges, labels=bin_labels, right=False)
+            binned = all_club_chart.groupby('CarryBin', observed=True).agg({'AoA': 'mean'}).reset_index()
 
-                fig3 = px.bar(
-                binned,
-                x='CarryBin',
-                y='AoA',
-                color='CarryBin',
-                labels={'CarryBin': 'Carry Distance (Yard Bins)', 'AoA': 'Average AoA'},
-                title=''
-            )
-                fig3.update_yaxes(range = aoa_range)
-                st.plotly_chart(fig3, use_container_width=True)
+            fig3 = px.bar(
+            binned,
+            x='CarryBin',
+            y='AoA',
+            color='CarryBin',
+            labels={'CarryBin': 'Carry Distance (Yard Bins)', 'AoA': 'Average AoA'},
+            title=''
+        )
+            fig3.update_yaxes(range = aoa_range)
+            st.plotly_chart(fig3, use_container_width=True)
+
+    ############## RENDER TAB 3 - SMASH FACTOR ANALYSIS ############## 
+
+    def render_smash_tab(self):
+        st.markdown(" ## Smash Factor Analysis")
+        all_club_chart = self.filter_data(self.date_range, 'All') 
+        selected_clubs = self.club_multiselect("smash_chart_club_filter")
+        filtered_chart = all_club_chart[all_club_chart['Club'].isin(selected_clubs)]
+
+        #create bins for smash factor
+        bin_edges = [0.5, 1.0, 1.1, 1.15, 1.2, 1.25, 1.3, 1.35, 1.4, 1.45, 1.5, 1.55, 1.6]
+        bin_labels = ['0.5–1', '1–1.1', '1.1-1.15', '1.15-1.2','1.2-1.25', '1.25-1.3', '1.3–1.35', '1.35–1.4', '1.4-1.45', '1.45-1.5', '1.5-1.55', '1.55-1.6']
+        filtered_chart = filtered_chart.copy()
+        filtered_chart['SmashBin'] = pd.cut(filtered_chart['SmashFactor'], bins=bin_edges, labels=bin_labels, right=False)
+        binned = filtered_chart.groupby('SmashBin', observed=True).agg({'DistanceToPin': 'mean'}).reset_index()
+
+        fig3 = px.bar(
+        binned,
+        x='SmashBin',
+        y='DistanceToPin',
+        color='SmashBin',
+        labels={'SmashBin': 'Smash Factor (ranges)', 'DistanceToPin': 'Average DistanceToPin'},
+        title=''
+    )
+        st.plotly_chart(fig3, use_container_width=True)
+
+
+
+
+
+
+
+
+
+    # def render_spin_tab():
+        # col_filters = st.columns(1)
+        # with col_filters[0]:
+        #     self.selected_club = st.selectbox("Filter by Club", self.club_filter_opts, index=0, key='club_filter_tab3')
+
+        # with col_filters[1]:
+        #     self.selected_spin = st.selectbox("Select Metric", self.spin_metrics, key='selected_spin_inline')
+
+        # filtered_data = self.filter_data(self.date_range, self.selected_club)  #filter data based on selections
+
+        # cols = st.columns((4,4), gap ='medium')
+        # with cols[0]:
+
+
+
+
 
 
 
